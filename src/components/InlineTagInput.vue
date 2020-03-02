057df4c2bf4a80176173558447a6d1c91fd814df
Alex Reznichenko <alex.reznichenko789@gmail.com>
2020-03-02 18:16:40 +0200

    Add Renderless example

---
+++ src/components/InlineTagInput.vue
@@ -0,0 +1,25 @@
+<template>
+  <renderless-tag-input :tags="tags" v-on:update="(newTags) => $emit('update', newTags)">
+    <div class="tag-input" slot-scope="{tags, removeTagEvents, inputProps, inputEvents}">
+      <span v-for="tag in tags" :key="tag" class="tag-input-tag">
+        <span>{{tag}}</span>
+        <button type="button" class="tag-input-remove" v-on="removeTagEvents(tag)">&times;</button>
+      </span>
+      <input class="tag-input-text" placeholder="Add tag..." v-bind="inputProps" v-on="inputEvents" />
+    </div>
+  </renderless-tag-input>
+</template>
+
+<script>
+import RenderlessTagInput from "@/components/RenderlessTagInput.vue";
+export default {
+  model: {
+    prop: "tags",
+    event: "update"
+  },
+  props: ["tags"],
+  components: {
+    RenderlessTagInput
+  }
+};
+</script>
---
+++ src/views/Renderless.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app" class="min-h-screen bg-grey-darker p-8">
+    <div class="max-w-sm mx-auto card mt-8">
+      <label class="form-label mb-2">Renderless Tag Input Alternate layout</label>
+      <renderless-tag-input v-model="tags">
+        <div class="stacked-tag-inut" slot-scope="{tags, removeTagEvents, inputProps, inputEvents}">
+          <div class="stacked-tag-input-form">
+            <input
+              v-bind="inputProps"
+              v-on="inputEvents"
+              placeholder="Add tag..."
+              class="form-input"
+            />
+            <button v-on="{click: inputEvents}" class="btn btn-indigo">Add tag</button>
+          </div>
+          <ul class="stacked-tag-list">
+            <li v-for="tag in tags" :key="tag">
+              {{tag}}
+              <button v-on="removeTagEvents(tag)" class="stacked-tag-link">Remove</button>
+            </li>
+          </ul>
+        </div>
+      </renderless-tag-input>
+    </div>
+    <div class="max-w-sm mx-auto card mt-8">
+      <label class="form-label mb-2">Renderless Tag Input</label>
+      <inline-tag-input v-model="tags"></inline-tag-input>
+    </div>
+    <div class="max-w-sm mx-auto card mt-8">
+      <label class="form-label mb-2">Original Tag Input</label>
+      <tag-input v-model="tags"></tag-input>
+    </div>
+  </div>
+</template>
+
+<script>
+import TagInput from "@/components/TagInput.vue";
+import RenderlessTagInput from "@/components/RenderlessTagInput.vue";
+import InlineTagInput from "@/components/InlineTagInput";
+
+export default {
+  components: {
+    TagInput,
+    RenderlessTagInput,
+    InlineTagInput
+  },
+  data() {
+    return {
+      tags: ["awesome", "excellent", "amazing"]
+    };
+  }
+};
+</script>
+
+<style src="../assets/app.css"/>
